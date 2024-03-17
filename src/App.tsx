1669466f7d87a34ab6e59966783bf73369ce05a3
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-03-16 22:08:34 -0700

    asset listing and asset details page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import PublishFormPage from './pages/PublishAssetPage';
 import Header from './header/header.component';
 import Sidebar from './sidebar/sidebar.component';
+import AssetDetailsPage from './pages/AssetDetailsPage';
 import { ToastContainer } from 'react-toastify';
 
 interface IAccountContext {
@@ -35,6 +36,7 @@ const App = () => {
                                     <Routes>
                                         <Route path="/" element={<LandingPage />} />
                                         <Route path="publish" element={<PublishFormPage />} />
+                                        <Route path="/details/:id" element={<AssetDetailsPage />} /> {/* Ensure this route is defined */}
                                     </Routes>
                             </div>
                         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,43 @@
 // src/pages/LandingPage.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+interface DataItem {
+  _id: string;
+  DataName: string;
+  Desc: string;
+  owneraddress: string;
+  EncryptedBytes: string;
+  sampleData: string;
+  Conditions: string;
+  __v: number;
+ }
 
 const LandingPage: React.FC = () => {
+  const [dataItems, setDataItems] = useState<DataItem[]>([]);
+  const navigate = useNavigate();
+ useEffect(() => {
+    // Replace 'yourApiUrl' with the actual URL of your dataItems API
+    fetch('http://localhost:5001/api/dataItems')
+      .then(response => response.json())
+      .then(data => setDataItems(data.slice(0, 10))) // Get top 10 items
+      .catch(error => console.error('Error fetching data:', error));
+ }, []);
+
+ const handleCardClick = (item: DataItem) => {
+  // Navigate to a new page, passing the item's _id as a parameter
+  navigate(`/details/${item._id}`);
+};
+
   return (
     <div>
-      <h1>Welcome to My App</h1>
-      {/* Add other elements as needed */}
+      {dataItems.map(item => (
+        <div key={item._id} className="card" onClick={() => handleCardClick(item)}>
+          <h3>{item.DataName}</h3>
+          <p>{item.Desc}</p>
+        </div>
+      ))}
     </div>
   );
 };
